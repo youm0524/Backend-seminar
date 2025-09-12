ec30e9e0225fe4744bf28ed0d9ef6c79a6374e17
youm <juliet0524@naver.com>
2025-09-12 14:13:38 +0900

    "feat:chatClient사용"

--- src/main/java/com/example/springAI/ChatController.java
+++ src/main/java/com/example/springAI/ChatController.java
@@ -2,6 +2,7 @@ package com.example.springAI;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class ChatController {
     private final ChatService chatService;
 
     @GetMapping("/ask")
-    public String ask(@RequestParam String message) {
+    public String ask(@RequestParam("message") String message) {
         return chatService.askToOllama(message);
     }
 
--- src/main/java/com/example/springAI/ChatService.java
+++ src/main/java/com/example/springAI/ChatService.java
@@ -2,6 +2,7 @@ package com.example.springAI;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
@@ -16,21 +17,39 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatService {
 
-    private final OllamaChatModel chatModel;
-
+     private final OllamaChatModel chatModel;
+//        private final ChatClient chatClient;
 
     public String askToOllama(String message) {
-//        Prompt prompt = new Prompt(
-//                List.of(
-//                        new SystemMessage("너는 친절한 AI 비서야. 절대 추론 과정을 보여주지 말고 요약만 응답해."),
-//                        new UserMessage(message)
-//                ));
-        String response = chatModel.call(message);
+        Prompt prompt = new Prompt(
+                List.of(
+                        new SystemMessage("너는 친절한 AI 비서야. 절대 추론 과정을 보여주지 말고 요약만 응답해. 답변은 반드시 한국어로만 해야 하며, 영어 표현을 사용하지 마. 단답으로 하지말라고"),
+                        new UserMessage(message)
+                ));
+
+        return ChatClient.create(chatModel)
+                .prompt(prompt)
+                .call()
+                .content();
+        //String response = String.valueOf(chatModel.call(prompt));
         //String answer = String.valueOf(response.getResult().getOutput());
-        return response;
+        //return response;
         //System.out.println(response.getResult().getOutput());
 
 //        return chatModel
 //                .call(prompt);    // Prompt 객체를 그대로 사용 ;// 응답 메시지 컨테이너
     }
+
+//    public String askToOllama(String message){
+//        Prompt prompt = new Prompt(
+//                List.of(
+//                        new SystemMessage("너는 친절한 AI 비서야. 절대 추론 과정을 보여주지 말고 요약만 응답해."),
+//                        new UserMessage(message)
+//                ));
+//
+//        return chatClient.prompt(prompt)
+//                .call()
+//                .content();
+//
+//    }
 }
